2e7250f0f5e65608efe98c2082fef0fc5954ef4e
aayushi1662 <67185267+aayushi1662@users.noreply.github.com>
2023-05-28 12:22:21 +0000

    Initial commit

---
+++ django_project/views.py
@@ -0,0 +1,109 @@
+from django.shortcuts import redirect
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+from django.shortcuts import render
+import os
+
+
+os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
+# This variable(av) specifies the name of a file that contains the OAuth 2.0
+CLIENT_SECRETS_FILE = r"C:\Users\AAYUSHI\OneDrive\Desktop\convin\convin_assignment\convin_assignment\av.json"
+
+# This OAuth 2.0 access scope allows for full read/write access to the authenticated user's account 
+SCOPES = ['https://www.googleapis.com/auth/calendar',
+          'https://www.googleapis.com/auth/userinfo.email',
+          'https://www.googleapis.com/auth/userinfo.profile',
+          'openid']
+REDIRECT_URL = 'http://127.0.0.1:8000/rest/v1/calendar/redirect'
+API_SERVICE_NAME = 'calendar'
+API_VERSION = 'v3'
+
+
+@api_view(['GET'])
+def GoogleCalendarInitView(request):
+    # Create flow instance to manage the OAuth 2.0 Authorization Grant Flow steps.
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES)
+
+    # The URI created here must exactly match one of the authorized redirect URIs
+    # for the OAuth 2.0 client, which you configured in the API Console. If this
+    # value doesn't match an authorized URI, you will get a 'redirect_uri_mismatch'
+    # error.
+    flow.redirect_uri = REDIRECT_URL
+
+    authorization_url, state = flow.authorization_url(
+        # Enable offline access so that you can refresh an access token without
+        # re-prompting the user for permission. Recommended for web server apps.
+        access_type='offline',
+        
+        include_granted_scopes='true')
+    request.session['state'] = state
+
+    return Response({"authorization_url": authorization_url})
+
+
+@api_view(['GET'])
+def GoogleCalendarRedirectView(request):
+    # Specify the state when creating the flow in the callback so that it can
+    # verified in the authorization server response.
+    state = request.session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    flow.redirect_uri = REDIRECT_URL
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = request.get_full_path()
+    flow.fetch_token(authorization_response=authorization_response)
+
+
+    # Save credentials back to session in case access token was refreshed.
+    
+    credentials = flow.credentials
+    request.session['credentials'] = credentials_to_dict(credentials)
+
+    # Check if credentials are in session
+    if 'credentials' not in request.session:
+        return redirect('v1/calendar/init')
+
+    # Load credentials from the session.
+    credentials = google.oauth2.credentials.Credentials(
+        **request.session['credentials'])
+
+    # Use the Google API Discovery Service to build client libraries, IDE plugins,
+    # and other tools that interact with Google APIs.
+    service = googleapiclient.discovery.build(
+        API_SERVICE_NAME, API_VERSION, credentials=credentials)
+
+    
+    calendar_list = service.calendarList().list().execute()
+
+    
+    calendar_id = calendar_list['items'][0]['id']
+
+    
+    events  = service.events().list(calendarId=calendar_id).execute()
+
+    events_list_append = []
+    if not events['items']:
+        print('No data found.')
+        return Response({"message": "No data found or user credentials invalid."})
+    else:
+        for events_list in events['items']:
+            events_list_append.append(events_list)
+            return Response({"events": events_list_append})
+    return Response({"error": "calendar event aren't here"})
+
+
+def credentials_to_dict(credentials):
+  return {'token': credentials.token,
+          'refresh_token': credentials.refresh_token,
+          'token_uri': credentials.token_uri,
+          'client_id': credentials.client_id,
+          'client_secret': credentials.client_secret,
+          'scopes': credentials.scopes}
